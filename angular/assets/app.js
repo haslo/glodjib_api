140817f4ec6ea71024a6e4805ab574867b88c70b
Guido Gloor <haslo@haslo.ch>
2015-03-03 00:23:21 +0100

    First data binding

--- angular/assets/app.js
+++ angular/assets/app.js
@@ -2,10 +2,17 @@
   var app = angular.module('glodjib', []);
 
   app.controller('HomeController', function() {
-    this.coverImage = image;
+    this.coverImages = images;
   });
 
-  var image = {
-    url: 'http://farm8.staticflickr.com/7287/8741489997_fe247035e8_o.jpg'
-  };
+  var images = [
+    {
+      title: 'colors',
+      url: 'http://farm8.staticflickr.com/7287/8741489997_fe247035e8_o.jpg'
+    },
+    {
+      title: 'fog',
+      url: 'http://farm9.staticflickr.com/8190/8447738544_77c0635190_o.jpg'
+    }
+  ];
 })();
